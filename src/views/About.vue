140fb2bb664b7b254296aae9a7aceb8c68b6b82d
yumikim <cjnmgirl@naver.com>
2018-11-22 17:00:52 +0900

    중간저장

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,18 +3,22 @@
     <v-tabs 
       left
       hide-slider
+      class="mainTabs"
     >
-      <v-tab :ripple="false">
+      <v-tab :ripple="false" class="mainTab">
         Nam mi kyung (ux planner)
       </v-tab>
-      <v-tab :ripple="false">
+      <v-tab :ripple="false" class="mainTab">
         Yu eun hye (designer)
       </v-tab>
       <v-tab :ripple="false">
         Kim yu mi (publisher)
       </v-tab>
       <v-tab-item>
-        123
+        <ul>
+          <li v-for="(item, i) in portfolio" :key="i">{{item.title}}</li>
+        </ul>
+        <Detail/>
       </v-tab-item>
       <v-tab-item>
         123
@@ -33,6 +37,15 @@ export default {
   components: {},
   data () {
     return {
+      portfolio: [{
+        title: '우리은행 위비톡 PC application 1.0',
+        client: '우리은행',
+        url: '/'
+      }, {
+        title: '우리은행 위비톡 PC application 1.0',
+        client: '우리은행',
+        url: '/'
+      }]
     }
   }
 }
