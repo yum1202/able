6d345404286adbe3e8f86cbabbb4e2a96c70a6d2
yumikim <cjnmgirl@naver.com>
2018-11-22 17:36:21 +0900

    중간저장

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,10 +15,20 @@
         Kim yu mi (publisher)
       </v-tab>
       <v-tab-item>
-        <ul>
-          <li v-for="(item, i) in portfolio" :key="i">{{item.title}}</li>
-        </ul>
-        <Detail/>
+        <v-layout row wrap>
+        <v-flex xs12 sm4>
+          <ul class="">
+            <li v-for="(item, i) in portfolio" :key="i" @click="handleDetail(i)">{{item.title}}</li>
+          </ul>
+        </v-flex>
+        <v-flex xs12 sm8>
+          <div class="detail">
+            {{correntTitle}}
+            {{correntClient}}
+            {{currentPath}}
+          </div>
+        </v-flex>
+        </v-layout>
       </v-tab-item>
       <v-tab-item>
         123
@@ -31,7 +41,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'about',
   components: {},
@@ -42,10 +51,40 @@ export default {
         client: '우리은행',
         url: '/'
       }, {
-        title: '우리은행 위비톡 PC application 1.0',
-        client: '우리은행',
+        title: '고영테크놀러지 OCT 의료 장비',
+        client: '우리은행2',
         url: '/'
-      }]
+      }, {
+        title: '고영테크놀러지 AOI 장비',
+        client: '우리은행2',
+        url: '/'
+      }, {
+        title: 'SK Broadband 옥수수 Mobile',
+        client: '우리은행2',
+        url: '/'
+      }, {
+        title: '라이프스타일 쇼핑몰 Urbanchild',
+        client: '우리은행2',
+        url: '/'
+      }, {
+        title: 'Video editing app 롤리캠',
+        client: '우리은행2',
+        url: '/'
+      }, {
+        title: '자동차 소셜네트워크 CARnect',
+        client: '우리은행2',
+        url: '/'
+      }],
+      correntTitle: '',
+      correntClient: '',
+      currentPath: ''
+    }
+  },
+  methods: {
+    handleDetail(i) {
+      this.correntTitle = this.portfolio[i].title
+      this.correntClient = this.portfolio[i].client
+      this.currentPath = this.portfolio[i].url
     }
   }
 }
